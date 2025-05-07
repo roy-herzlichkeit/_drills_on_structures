e1fae99bf7734d5bcf241e5343916b125670ae70
Souharda Roy <b123134@iiit-bh.ac.in>
2025-05-07 20:32:16 +0530

    commit-XIX

---
+++ BINTREE.h
@@ -0,0 +1,134 @@
+#ifndef BINTREE_H
+#define BINTREE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <math.h>
+
+typedef struct BINARY_TREE_STRUCTURE {
+    int val;
+    struct BINARY_TREE_STRUCTURE *left, *right;
+} node;
+
+node* create_node(int);
+void inorder(node*, char*, int);
+void preorder(node*, char*, int);
+void postorder(node*, char*, int);
+node* read(char*);
+void write(char*, node*);
+node* make_phi(char*, char*);
+node* make_psi(char*, char*);
+
+node* create_node(int val) {
+    node* nptr = (node*)malloc(sizeof(node));
+    if (!nptr) return NULL;
+    nptr->val = val;
+    nptr->left = nptr->right = NULL;
+    return nptr;
+}
+
+void inorder(node* root, char* s, int i) {
+    if (root == NULL) return;
+    inorder(root->left, s, i);
+    s[i++] = root->val;
+    inorder(root->right, s, i);
+}
+
+void preorder(node* root, char* s, int i) {
+    if (root == NULL) return;
+    s[i++] = root->val;
+    preorder(root->left, s, i);
+    preorder(root->right, s, i);
+}
+
+void postorder(node* root, char* s, int i) {
+    if (root == NULL) return;
+    s[i++] = root->val;
+    postorder(root->left, s, i);
+    postorder(root->right, s, i);
+}
+
+void write(char *dest, node *root) {
+    FILE *fps = fopen(dest, "w");
+    if (!fps) return;
+    char *s_in = (char*)calloc(128, 1);
+    char *s_pre = (char*)calloc(128, 1);
+    char *s_post = (char*)calloc(128, 1);
+    inorder(root, s_in, 0);
+    preorder(root, s_pre, 0);
+    postorder(root, s_post, 0);
+    for (int i = 0; s_in[i] != '\0'; i++) fprintf(fps, "%d ", s_in[i]);
+    fprintf(fps, "\n");
+    for (int i = 0; s_pre[i] != '\0'; i++) fprintf(fps, "%d ", s_pre[i]);
+    fprintf(fps, "\n");
+    for (int i = 0; s_post[i] != '\0'; i++) fprintf(fps, "%d ", s_post[i]);
+    fprintf(fps, "\n");
+    fclose(fps);
+    free(s_in);
+    free(s_pre);
+    free(s_post);
+}
+
+node* read(char *src) {
+    FILE *fps = fopen(src, "r");
+    if (!fps) return NULL;
+    node *dummy = create_node(0), *tail = dummy;
+    char *s1 = (char*)malloc(100), *s2 = (char*)malloc(100);
+    int num;
+    fscanf(fps, "%d\n", &num);
+    fgets(s1, 100, fps);
+    fgets(s2, 100, fps);
+    node *res = num == 0 ? make_phi(s1, s2) : make_psi(s1, s2);
+    fclose(fps);
+    free(s1);
+    free(s2);
+    free(dummy);
+    return res;
+}
+
+static node* build_tree_phi(const char* in, int in_lo, int in_hi, const char* pre, int pre_lo, int pre_hi) {
+    if (pre_lo > pre_hi || in_lo > in_hi) return NULL;
+    int root_val = (int)pre[pre_lo];
+    node* root = (node*)malloc(sizeof(node));
+    if (!root) return NULL;
+    root->val = root_val;
+    root->left = root->right = NULL;
+    int i = in_lo;
+    while (i <= in_hi && (int)in[i] != root_val) ++i;
+    int left_size = i - in_lo;
+    root->left = build_tree_phi(in, in_lo, i - 1, pre, pre_lo + 1, pre_lo + left_size);
+    root->right = build_tree_phi(in, i + 1, in_hi, pre, pre_lo + left_size + 1, pre_hi);
+    return root;
+}
+
+node* make_phi(char* s_in, char* s_pre) {
+    if (!s_in || !s_pre || s_pre[0] == '\0') return NULL;
+    int n = 0;
+    while (s_pre[n]) ++n;
+    return build_tree_phi(s_in, 0, n - 1, s_pre, 0, n - 1);
+}
+
+static node* build_tree_psi(const char* in, int in_lo, int in_hi, const char* post, int post_lo, int post_hi) {
+    if (post_lo > post_hi || in_lo > in_hi) return NULL;
+    int root_val = (int)post[post_hi];
+    node* root = (node*)malloc(sizeof(node));
+    if (!root) return NULL;
+    root->val = root_val;
+    root->left = root->right = NULL;
+    int i = in_lo;
+    while (i <= in_hi && (int)in[i] != root_val) ++i;
+    int left_size = i - in_lo;
+    root->left = build_tree_psi(in, in_lo, i - 1, post, post_lo, post_lo + left_size - 1);
+    root->right = build_tree_psi(in, i + 1, in_hi, post, post_lo + left_size, post_hi - 1);
+    return root;
+}
+
+node* make_psi(char* s_in, char* s_post) {
+    if (!s_in || !s_post || s_post[0] == '\0') return NULL;
+    int n = 0;
+    while (s_post[n]) ++n;
+    return build_tree_psi(s_in, 0, n - 1, s_post, 0, n - 1);
+}
+
+#endif
